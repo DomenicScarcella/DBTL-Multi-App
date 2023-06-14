f0b804690a27d42d2b9ac5207335681776e4ccbf
unknown <ls41513@yahoo.com>
2023-06-14 05:27:59 -0400

    Radio buttons for Modular selection

--- src/Components/Modular.jsx
+++ src/Components/Modular.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import { BracketModular } from "../Bracket/BracketModular";
 import { ListModular } from "../List/ListModular";
 import "./Components.css";
 
-
 export function Modular() {
+    const [modtype, setModtype] = useState("nullbg");
+
+    const radioChange = (e) => {
+        setModtype(e.target.value);
+    }
+
     return (
         <div className="modular">
-            {/* <div id="nullbg" /> */}
-            <div id="whitebg" />
-            {/* <BracketModular /> */}
-            {/* <ListModular /> */}
+            <div className="modular-selector">
+                <div>
+                    <input type="radio" value="nullbg" checked={modtype === "nullbg"} onChange={radioChange} />BLANK (default)
+                </div>
+                <div>
+                    <input type="radio" value="whitebg" checked={modtype === "whitebg"} onChange={radioChange} />Plain White
+                </div>
+                <div>
+                    <input type="radio" value="bracket" checked={modtype === "bracket"} onChange={radioChange} />Bracket Reveal
+                </div>
+                <div>
+                    <input type="radio" value="list" checked={modtype === "list"} onChange={radioChange} />List Reveal
+                </div>
+            </div>
+            {
+                (modtype === "whitebg")
+                    ? <div id="whitebg" />
+                    : (modtype === "bracket")
+                        ? <BracketModular />
+                        : (modtype === "list")
+                            ? <ListModular />
+                            : <div id="nullbg" />
+            }
         </div>
     )
 }
