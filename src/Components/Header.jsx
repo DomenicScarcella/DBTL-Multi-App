102b2952723a9abab4d07a49ef81bd26f47f890e
unknown <ls41513@yahoo.com>
2023-07-01 01:33:08 -0400

    Add ids to h1 in Header.jsx for easier style tweaks

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,27 +3,32 @@ import "./Components.css";
 
 export function Header() {
     const [index, setIndex] = useState(0);
+    const dbtl = "Don't Bury The Lead";
     const headTypes = ["RECAP", "PREVIEW", "SPECIAL"];
     const len = headTypes.length;
   
     const handlePrev = () => {
-      setIndex(index <= 0 ? len - 1 : index - 1);
+      setIndex(index == 0 ? len - 1 : index - 1);
     };
   
     const handleNext = () => {
-      setIndex(index >= len - 1 ? 0 : index + 1);
+      setIndex(index == len - 1 ? 0 : index + 1);
     };
 
     return (
         <div className="header">
-            <h1>Don't Bury The Lead</h1>
+            <h1 id="dbtl">
+                { dbtl }
+            </h1>
             <div className="carousel">
                 <button className="carouselButton" onClick={handlePrev}>
-                    <br />{index <= 0 ? headTypes[len - 1] : headTypes[index - 1]}<br /><br /> {`<<<`} 
+                    <br />{index == 0 ? headTypes[len - 1] : headTypes[index - 1]}<br /><br /> {`<<<`} 
                 </button>
-                <h1>{headTypes[index]}</h1>
+                <h1 id="headtype">
+                    { headTypes[index] }
+                </h1>
                 <button  className="carouselButton" onClick={handleNext}>
-                    <br />{index >= len - 1 ? headTypes[0] : headTypes[index + 1]}<br /><br /> {`>>>`}
+                    <br />{index == len - 1 ? headTypes[0] : headTypes[index + 1]}<br /><br /> {`>>>`}
                 </button>
             </div>
         </div>
