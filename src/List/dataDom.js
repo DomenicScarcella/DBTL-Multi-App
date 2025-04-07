8533f8e21f861bec10bff109204b38d62ee20def
unknown <ls41513@yahoo.com>
2025-04-06 22:01:29 -0400

    First 5 updates to dataDom&John

--- src/List/dataDom.js
+++ src/List/dataDom.js
@@ -1,33 +1,22 @@
 import * as mp from "./art/matchphotos/mp-index.js";
 
-const a = {title: "Roman Reigns (c) vs. Cody Rhodes",
-                pix: mp.ROMAN_CODY, vis: "hide-li"};
-const b = {title: "Roman Reigns & The Rock vs. Cody Rhodes & Seth Rollins",
-                pix: mp.TAG_RR_CR, vis: "hide-li"};
-const c = {title: "Seth Rollins (c) vs. Drew McIntyre",
-                pix: mp.ROLLINS_MCINTYRE, vis: "hide-li"};
-const d = {title: "Rhea Ripley (c) vs. Becky Lynch",
-                pix: mp.RHEA_BECKY, vis: "hide-li"};
-const e = {title: "Iyo Sky (c) vs. Bayley",
-                pix: mp.IYO_BAYLEY, vis: "hide-li"};
-const f = {title: "Logan Paul (c) vs. Kevin Owens vs. Randy Orton",
-                pix: mp.US_3, vis: "hide-li"};
-const g = {title: "Gunther (c) vs. Sami Zayn",
-                pix: mp.GUNTHER_SAMI, vis: "hide-li"};                
-const h = {title: "LA Knight vs. AJ Styles",
-                pix: mp.AJ_LA, vis: "hide-li"};
-const i = {title: "Jey Uso vs. Jimmy Uso",
-                pix: mp.USOS, vis: "hide-li"};
-const j = {title: "Men's Tag Team Championship Ladder Match",
-                pix: mp.LADDER, vis: "hide-li"};
-const k = {title: "Bianca, Jade, Naomi vs. Damage CTRL",
-                pix: mp.WOMENS_TAG, vis: "hide-li"};
+const a = {title: "Cody Rhodes (c) vs. John Cena",
+                pix: mp.CODY_CENA, vis: "hide-li"};
+const b = {title: "Gunther (c) vs. Jey Uso",
+                pix: mp.GUNTHER_JEY, vis: "hide-li"};
+const c = {title: "LA Knight (c) vs. Jacob Fatu",
+                pix: mp.LA_FATU, vis: "hide-li"};
+const d = {title: "Tiffany Stratton (c) vs. Charlotte Flair",
+                pix: mp.TIFF_CHAR, vis: "hide-li"};
+const e = {title: "AJ Styles vs. Logan Paul",
+                pix: mp.AJ_LOGAN, vis: "hide-li"};
+
 
 const Rheader = {title: "Dom's List:"};
-const R1 = g;
-const R2 = h;
-const R3 = a;
-const R4 = e;
-const R5 = d;
+const R1 = a;
+const R2 = b;
+const R3 = c;
+const R4 = d;
+const R5 = e;
 
 export const listR = [Rheader, R1, R2, R3, R4, R5];
--- src/List/dataJohn.js
+++ src/List/dataJohn.js
@@ -1,33 +1,21 @@
 import * as mp from "./art/matchphotos/mp-index.js";
 
-const a = {title: "Roman Reigns (c) vs. Cody Rhodes",
-                pix: mp.ROMAN_CODY, vis: "hide-li"};
-const b = {title: "Roman Reigns & The Rock vs. Cody Rhodes & Seth Rollins",
-                pix: mp.TAG_RR_CR, vis: "hide-li"};
-const c = {title: "Seth Rollins (c) vs. Drew McIntyre",
-                pix: mp.ROLLINS_MCINTYRE, vis: "hide-li"};
-const d = {title: "Rhea Ripley (c) vs. Becky Lynch",
-                pix: mp.RHEA_BECKY, vis: "hide-li"};
-const e = {title: "Iyo Sky (c) vs. Bayley",
-                pix: mp.IYO_BAYLEY, vis: "hide-li"};
-const f = {title: "Logan Paul (c) vs. Kevin Owens vs. Randy Orton",
-                pix: mp.US_3, vis: "hide-li"};
-const g = {title: "Gunther (c) vs. Sami Zayn",
-                pix: mp.GUNTHER_SAMI, vis: "hide-li"};                
-const h = {title: "LA Knight vs. AJ Styles",
-                pix: mp.AJ_LA, vis: "hide-li"};
-const i = {title: "Jey Uso vs. Jimmy Uso",
-                pix: mp.USOS, vis: "hide-li"};
-const j = {title: "Men's Tag Team Championship Ladder Match",
-                pix: mp.LADDER, vis: "hide-li"};
-const k = {title: "Bianca, Jade, Naomi vs. Damage CTRL",
-                pix: mp.WOMENS_TAG, vis: "hide-li"};
+const a = {title: "Cody Rhodes (c) vs. John Cena",
+    pix: mp.CODY_CENA, vis: "hide-li"};
+const b = {title: "Gunther (c) vs. Jey Uso",
+    pix: mp.GUNTHER_JEY, vis: "hide-li"};
+const c = {title: "LA Knight (c) vs. Jacob Fatu",
+    pix: mp.LA_FATU, vis: "hide-li"};
+const d = {title: "Tiffany Stratton (c) vs. Charlotte Flair",
+    pix: mp.TIFF_CHAR, vis: "hide-li"};
+const e = {title: "AJ Styles vs. Logan Paul",
+    pix: mp.AJ_LOGAN, vis: "hide-li"};
 
 const Lheader = {title: "John's List:"};
-const L1 = h;
-const L2 = b;
-const L3 = e;
-const L4 = i;
+const L1 = e;
+const L2 = d;
+const L3 = c;
+const L4 = b;
 const L5 = a;
 
 export const listL = [Lheader, L1, L2, L3, L4, L5];
