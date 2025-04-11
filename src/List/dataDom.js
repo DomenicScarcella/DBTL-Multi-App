25a0fbc15eeef82c376a79282c4c8776263211e5
unknown <ls41513@yahoo.com>
2025-04-11 18:05:17 -0400

    12 total matches w graphics

--- src/List/dataDom.js
+++ src/List/dataDom.js
@@ -4,26 +4,32 @@ const a = {title: "Cody Rhodes (c) vs. John Cena",
     pix: mp.CODY_CENA, vis: "hide-li"};
 const b = {title: "Gunther (c) vs. Jey Uso",
     pix: mp.GUNTHER_JEY, vis: "hide-li"};
-const c = {title: "LA Knight (c) vs. Jacob Fatu",
-    pix: mp.LA_FATU, vis: "hide-li"};
-const d = {title: "Bron Breakker (c) vs. Penta vs. Finn Bálor vs. Dominik Mysterio",
-    pix: mp.IC_4WAY, vis: "hide-li"};
-const e = {title: "Tiffany Stratton (c) vs. Charlotte Flair",
+const c = {title: "Tiffany Stratton (c) vs. Charlotte Flair",
     pix: mp.TIFF_CHAR, vis: "hide-li"};
-const f = {title: "Iyo Sky (c) vs. Rhea Ripley vs. Bianca Belair",
+const d = {title: "Iyo Sky (c) vs. Rhea Ripley vs. Bianca Belair",
     pix: mp.IYO_RHEA_BIANCA, vis: "hide-li"};
-const g = {title: "AJ Styles vs. Logan Paul",
+const e = {title: "LA Knight (c) vs. Jacob Fatu",
+    pix: mp.LA_FATU, vis: "hide-li"};
+const f = {title: "Bron Breakker (c) vs. Penta vs. Finn Bálor vs. Dominik Mysterio",
+    pix: mp.IC_4WAY, vis: "hide-li"};
+const g = {title: "War Raiders (c) vs. New Day",
+    pix: mp.WR_ND, vis: "hide-li"};
+const h = {title: "Seth Rollins vs. Roman Reigns vs. CM Punk",
+    pix: mp.SR_RR_CMP, vis: "hide-li"};
+const i = {title: "AJ Styles vs. Logan Paul",
     pix: mp.AJ_LOGAN, vis: "hide-li"};
-const h = {title: "Jade Cargill vs. Naomi",
+const j = {title: "Jade Cargill vs. Naomi",
     pix: mp.JADE_NAOMI, vis: "hide-li"};
-const i = {title: "Rey Mysterio vs. El Grande Americano",
+const k = {title: "Rey Mysterio vs. El Grande Americano",
     pix: mp.REY_GRANDE, vis: "hide-li"};
+const l = {title: "Damian Priest vs. Drew McIntyre",
+    pix: mp.DAMIEN_DREW, vis: "hide-li"};
 
 const Rheader = {title: "Dom's List:"};
-const R1 = d;
-const R2 = f;
-const R3 = h;
-const R4 = i;
-const R5 = e;
+const R1 = c;
+const R2 = d;
+const R3 = e;
+const R4 = f;
+const R5 = g;
 
 export const listR = [Rheader, R1, R2, R3, R4, R5];
--- src/List/dataJohn.js
+++ src/List/dataJohn.js
@@ -4,26 +4,32 @@ const a = {title: "Cody Rhodes (c) vs. John Cena",
     pix: mp.CODY_CENA, vis: "hide-li"};
 const b = {title: "Gunther (c) vs. Jey Uso",
     pix: mp.GUNTHER_JEY, vis: "hide-li"};
-const c = {title: "LA Knight (c) vs. Jacob Fatu",
-    pix: mp.LA_FATU, vis: "hide-li"};
-const d = {title: "Bron Breakker (c) vs. Penta vs. Finn Bálor vs. Dominik Mysterio",
-    pix: mp.IC_4WAY, vis: "hide-li"};
-const e = {title: "Tiffany Stratton (c) vs. Charlotte Flair",
+const c = {title: "Tiffany Stratton (c) vs. Charlotte Flair",
     pix: mp.TIFF_CHAR, vis: "hide-li"};
-const f = {title: "Iyo Sky (c) vs. Rhea Ripley vs. Bianca Belair",
+const d = {title: "Iyo Sky (c) vs. Rhea Ripley vs. Bianca Belair",
     pix: mp.IYO_RHEA_BIANCA, vis: "hide-li"};
-const g = {title: "AJ Styles vs. Logan Paul",
+const e = {title: "LA Knight (c) vs. Jacob Fatu",
+    pix: mp.LA_FATU, vis: "hide-li"};
+const f = {title: "Bron Breakker (c) vs. Penta vs. Finn Bálor vs. Dominik Mysterio",
+    pix: mp.IC_4WAY, vis: "hide-li"};
+const g = {title: "War Raiders (c) vs. New Day",
+    pix: mp.WR_ND, vis: "hide-li"};
+const h = {title: "Seth Rollins vs. Roman Reigns vs. CM Punk",
+    pix: mp.SR_RR_CMP, vis: "hide-li"};
+const i = {title: "AJ Styles vs. Logan Paul",
     pix: mp.AJ_LOGAN, vis: "hide-li"};
-const h = {title: "Jade Cargill vs. Naomi",
+const j = {title: "Jade Cargill vs. Naomi",
     pix: mp.JADE_NAOMI, vis: "hide-li"};
-const i = {title: "Rey Mysterio vs. El Grande Americano",
+const k = {title: "Rey Mysterio vs. El Grande Americano",
     pix: mp.REY_GRANDE, vis: "hide-li"};
+const l = {title: "Damian Priest vs. Drew McIntyre",
+    pix: mp.DAMIEN_DREW, vis: "hide-li"};
 
 const Lheader = {title: "John's List:"};
-const L1 = b;
-const L2 = d;
-const L3 = f;
-const L4 = h;
-const L5 = a;
+const L1 = h;
+const L2 = i;
+const L3 = j;
+const L4 = k;
+const L5 = l;
 
 export const listL = [Lheader, L1, L2, L3, L4, L5];
