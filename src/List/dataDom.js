6968e0c3ecc9f2d66c4e84f841da1ff9671667f5
unknown <ls41513@yahoo.com>
2025-04-11 18:45:31 -0400

    Re-order matches

--- src/List/dataDom.js
+++ src/List/dataDom.js
@@ -2,34 +2,45 @@ import * as mp from "./art/matchphotos/mp-index.js";
 
 const a = {title: "Cody Rhodes (c) vs. John Cena",
     pix: mp.CODY_CENA, vis: "hide-li"};
+
 const b = {title: "Gunther (c) vs. Jey Uso",
     pix: mp.GUNTHER_JEY, vis: "hide-li"};
+
 const c = {title: "Tiffany Stratton (c) vs. Charlotte Flair",
     pix: mp.TIFF_CHAR, vis: "hide-li"};
+
 const d = {title: "Iyo Sky (c) vs. Rhea Ripley vs. Bianca Belair",
     pix: mp.IYO_RHEA_BIANCA, vis: "hide-li"};
+
 const e = {title: "LA Knight (c) vs. Jacob Fatu",
     pix: mp.LA_FATU, vis: "hide-li"};
+
 const f = {title: "Bron Breakker (c) vs. Penta vs. Finn Bálor vs. Dominik Mysterio",
     pix: mp.IC_4WAY, vis: "hide-li"};
+
 const g = {title: "War Raiders (c) vs. New Day",
     pix: mp.WR_ND, vis: "hide-li"};
+
 const h = {title: "Seth Rollins vs. Roman Reigns vs. CM Punk",
     pix: mp.SR_RR_CMP, vis: "hide-li"};
+
 const i = {title: "AJ Styles vs. Logan Paul",
     pix: mp.AJ_LOGAN, vis: "hide-li"};
+
 const j = {title: "Jade Cargill vs. Naomi",
     pix: mp.JADE_NAOMI, vis: "hide-li"};
+
 const k = {title: "Rey Mysterio vs. El Grande Americano",
     pix: mp.REY_GRANDE, vis: "hide-li"};
+
 const l = {title: "Damian Priest vs. Drew McIntyre",
     pix: mp.DAMIEN_DREW, vis: "hide-li"};
 
 const Rheader = {title: "Dom's List:"};
-const R1 = c;
+const R1 = e;
 const R2 = d;
-const R3 = e;
-const R4 = f;
-const R5 = g;
+const R3 = a;
+const R4 = j;
+const R5 = h;
 
 export const listR = [Rheader, R1, R2, R3, R4, R5];
--- src/List/dataJohn.js
+++ src/List/dataJohn.js
@@ -2,34 +2,45 @@ import * as mp from "./art/matchphotos/mp-index.js";
 
 const a = {title: "Cody Rhodes (c) vs. John Cena",
     pix: mp.CODY_CENA, vis: "hide-li"};
+
 const b = {title: "Gunther (c) vs. Jey Uso",
     pix: mp.GUNTHER_JEY, vis: "hide-li"};
+
 const c = {title: "Tiffany Stratton (c) vs. Charlotte Flair",
     pix: mp.TIFF_CHAR, vis: "hide-li"};
+
 const d = {title: "Iyo Sky (c) vs. Rhea Ripley vs. Bianca Belair",
     pix: mp.IYO_RHEA_BIANCA, vis: "hide-li"};
+
 const e = {title: "LA Knight (c) vs. Jacob Fatu",
     pix: mp.LA_FATU, vis: "hide-li"};
+
 const f = {title: "Bron Breakker (c) vs. Penta vs. Finn Bálor vs. Dominik Mysterio",
     pix: mp.IC_4WAY, vis: "hide-li"};
+
 const g = {title: "War Raiders (c) vs. New Day",
     pix: mp.WR_ND, vis: "hide-li"};
+
 const h = {title: "Seth Rollins vs. Roman Reigns vs. CM Punk",
     pix: mp.SR_RR_CMP, vis: "hide-li"};
+
 const i = {title: "AJ Styles vs. Logan Paul",
     pix: mp.AJ_LOGAN, vis: "hide-li"};
+
 const j = {title: "Jade Cargill vs. Naomi",
     pix: mp.JADE_NAOMI, vis: "hide-li"};
+
 const k = {title: "Rey Mysterio vs. El Grande Americano",
     pix: mp.REY_GRANDE, vis: "hide-li"};
+    
 const l = {title: "Damian Priest vs. Drew McIntyre",
     pix: mp.DAMIEN_DREW, vis: "hide-li"};
 
 const Lheader = {title: "John's List:"};
-const L1 = h;
-const L2 = i;
-const L3 = j;
-const L4 = k;
-const L5 = l;
+const L1 = a;
+const L2 = b;
+const L3 = c;
+const L4 = d;
+const L5 = e;
 
 export const listL = [Lheader, L1, L2, L3, L4, L5];
